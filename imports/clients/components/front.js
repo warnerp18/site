521cb4fe80fbb250f8fcf1185a059f80213bb5e5
Phil <warnerp18@gmail.com>
2016-07-23 16:37:08 -0500

    remove insecure and autopublish

--- imports/clients/components/front.js
+++ imports/clients/components/front.js
@@ -15,32 +15,28 @@ export default class Front extends Component {
 
   render(){
     return (
-      <div>
-        <Menu />
-        <section className='header-image'>
-          <div className='text-container'>
-            <h1>I am Phil</h1><br />
-            <h2>I love to code & and backpack</h2>
-            <a href='#' onClick={this.scrollToNextSection}>read more</a>
-          </div>
-        </section>
-        <section className='aboutme'>
-          <p className='contain580 push60'>
-            <span>My</span>
-            <b> name is Phil Warner</b>.
-              <p>
-              I started programming two years ago. I attended the Iron Yard's Front End Engineering course in Houston, TX in September 2014. While attending The Iron Yard I learned the basics of HTML5, CSS3, and Javascript. I’m proficient in Drupal 7 and am building modules to support Drupal 8. Outside of work I have been teaching myself Meteor and React using resources such as egghead.io, leveluptutorials.com, themeteorchef.com, meteor.com, & facebook.github.io/react.
-              </p>
+    <div>
+      <Menu />
+      <section className='header-image'>
+        <div className='text-container'>
+                <h1>I am Phil</h1><br />
+                <h2>I love to code & and backpack</h2>
+                <a href='#' onClick={this.scrollToNextSection}>read more</a>
+        </div>
+      </section>
+      <section className='aboutme contain580 push60'>
+          <span>My</span>
+          <b> name is Phil Warner</b>.
+          <p>I started programming two years ago. I attended the Iron Yard's Front End Engineering course in Houston, TX in September 2014. While attending The Iron Yard I learned the basics of HTML5, CSS3, and Javascript. I’m proficient in Drupal 7 and am building modules to support Drupal 8. Outside of work I have been teaching myself Meteor and React using resources such as egghead.io, leveluptutorials.com, themeteorchef.com, meteor.com, & facebook.github.io/react.</p>
         <div className='seperator push60'></div>
         <div className='contain580'>
           <p>Like most developers that come out of a programming bootcamp, my path into the programing world started somewhere completely different.</p>
           <p>I grew up a military brat. I was born in a small German town and have since then been traveling the world. During my Sophomore year at The Citadel in Charleston, SC, I left for my first of two deployments to Afghanistan. After my second deployment I returned to The Citadel and graduated in 2010.</p>
           <p>I spent the next four years traveling to over 35 countries and working overseas. During my travels, I came across an article explaining the demand for developers and their impact in the future. This article pointed me to codecademy.com. My love for coding started at Codecademy and my desire to learn more has only grown.</p>
         </div>
-     </section>
-
+      </section>
       <Footer />
-      </div>
+    </div>
     )
   }
 }
