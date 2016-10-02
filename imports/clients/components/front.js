f8387f3127642a428d5ca6a14c806a20082df70a
Phil <warnerp18@gmail.com>
2016-10-02 18:37:14 -0500

    remove console.log

--- imports/clients/components/front.js
+++ imports/clients/components/front.js
@@ -18,8 +18,6 @@ export default class Front extends Component {
     setTimeout( () => {
       ELEM.map(items =>{
         className= items.className;
-        console.log(items);
-        debugger;
         items.className = `${className} open`;
       })
     }, 800)
